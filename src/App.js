eacb0f38bd5ab0742c923a8dd810edc4d16ba02e
indra <indra@lt024823.cts.com>
2018-07-27 16:07:27 +0530

    added Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { ToDoComponent } from './components//todo/toDoForm';
+import { WelcomeComponent } from './components/WelcomeComponent';
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <ToDoComponent />
+        <Link to="/route">To-Do-Component</Link>
+        <Route to="/" exact component={WelcomeComponent}/>
+        <Route path='/route' exact component={ToDoComponent} />
+       
       </div>
+      </Router>
     );
   }
 }
